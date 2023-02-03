ec539cab7bff8f6bcd1308fdd5f505f5956f570f
Jeanne04 <jane22jene04@icloud.com>
2023-02-03 12:50:49 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+let compteur = 0;
+let elements, slides, slideWidth;
+
+window.onload = () =>{
+    const diapo = document.querySelector(".diapo");
+    elements = document.querySelector(".elements");
+    let firstVideo = elements.firstElementChild.cloneNode(true);
+    elements.appendChild(firstVideo);
+    slides = Array.from(elements.children);
+    slideWidth = diapo.getBoundingClientRect().width;
+    let next = document.querySelector("#nav-droite");
+    let prev = document.querySelector("#nav-gauche");
+    next.addEventListener("click", slideNext);
+    prev.addEventListener("click", slidePrev);
+
+
+}
+
+function slideNext() {
+    compteur++;
+    elements.style.transition = " 1s linear";
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+    setTimeout(function () {
+        if (compteur >= slides.length - 1){
+            compteur =0;
+            elements.style.transition = "unset";
+            elements.style.transform = "translateX(0)";
+        }
+    },1000);
+}
+
+function slidePrev() {
+    compteur--;
+    elements.style.transition = "is linear";
+    if (compteur < 0){
+        compteur  = slides.length - 1;
+        let decal = -slideWidth * compteur;
+        elements.style.transition = "unset";
+        elements.style.transform = `translateX(${decal}px)`;
+        setTimeout(slidePrev, 1);
+    }
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+
+}
